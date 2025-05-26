ca841b8327be32459818d956d3a6bf4adcd86bff
Karol Wojtyfa <benedictus.karol@ti.ukdw.ac.id>
2025-05-26 10:35:21 +0700

    Karol

---
+++ Project_Kelompok_rplbo/src/main/java/projek_keuangan/data/TargetPengeluaran.java
@@ -0,0 +1,32 @@
+package projek_keuangan.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TargetPengeluaran {
+
+    private Map<String, Integer> pengeluaranPerBulan;
+    private int target;
+
+    public TargetPengeluaran(int target) {
+        this.target = target;
+        this.pengeluaranPerBulan = new HashMap<>();
+    }
+
+    // Mengatur target pengeluaran
+
+    public void tambahPengeluaran(String bulan, int jumlah) {
+        int total = pengeluaranPerBulan.getOrDefault(bulan, 0) + jumlah;
+        pengeluaranPerBulan.put(bulan, total);
+
+        if (total > target) {
+            System.out.println("[WARNING] Pengeluaran bulan " + bulan + " telah melebihi target: " + total);
+        } else {
+            System.out.println("Pengeluaran ditambahkan untuk " + bulan + ": " + total);
+        }
+    }
+
+    public Map<String, Integer> getPengeluaranPerBulan() {
+        return pengeluaranPerBulan;
+    }
+}
